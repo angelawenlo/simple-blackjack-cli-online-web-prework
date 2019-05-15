3c9efa5c348457e49d56a38aafab7e548c52c03b
Angela Lo <angelawenlo@gmail.com>
2019-05-15 14:38:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,7 +35,6 @@ def hit?(card_number)
   until user_input == 'h' || user_input == 's'
     invalid_command
     if user_input == 'h'
-      deal_card
 
     else
       display_card_total(card_number)
