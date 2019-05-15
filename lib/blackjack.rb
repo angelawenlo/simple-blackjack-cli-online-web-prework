a03dd01c13370321e734326226c4314be4fa84ed
Angela Lo <angelawenlo@gmail.com>
2019-05-15 14:22:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,7 +37,7 @@ def hit?(card_number)
     deal_card
     display_card_total(card_number)
     else
-      display_card_total(card_number)
+    return display_card_total(card_number)
     end
   end
 end
